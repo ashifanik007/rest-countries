b5a7b54b78724e11b280c85bade06d9842089388
ashifanik007 <mohammadashif.anik@gmail.com>
2022-03-16 07:46:07 +0600

    third commit

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const loadCountries = () => {
 const displayCountries = countries => {
     console.log(countries);
     const countriesHTML = countries.map(country => getCountryHTML(country));
-    console.log(countriesHTML);
+    // console.log(countriesHTML);
     const country = document.getElementById('countries');
     country.innerHTML = countriesHTML.join(' ');
 }
 
 const getCountryHTML = country => {
     return `
-        <div>
+        <div id="country">
             <h2>
                 ${country.name.common}
             </h2>
+            <img src='${country.flags.png}'>
         </div>
     `
 }
